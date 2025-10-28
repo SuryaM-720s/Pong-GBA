7bdfd73d2fcb99c9e3007d3fe9d5d4746ce85f2b
Suryaprakash Murugavvel <suryamvvel@gmail.com>
2025-10-28 15:56:04 -0400

    Replacing the incorrect main.cpp with the new pong.cpp code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
-/*
-    License for Butano:
-
-    Copyright (c) 2020-2022 Gustavo Valiente gustavo.valiente@protonmail.com
-    zlib License, see LICENSE file.
-*/
-
-// Butano libraries
-#include "bn_core.h" // Core libraries.
+#include "bn_core.h"
 #include "bn_log.h"
 #include "bn_sram.h"
 #include "bn_music.h"
 #include "bn_music_actions.h"
 #include "bn_music_items.h"
 #include "bn_sound_items.h"
-#include "bn_sram.h"
 #include "bn_math.h"
 #include "bn_string.h"
 #include "bn_keypad.h"
@@ -25,307 +16,174 @@
 #include "bn_sprite_palette_ptr.h"
 #include "common_info.h"
 #include "common_variable_8x8_sprite_font.h"
-
 #include "bn_sprite_items_paddle.h"
 #include "bn_sprite_items_ball.h"
-#include "bn_sprite_items_ball_new.h"
-#include "bn_sprite_items_paddle_new.h"
 #include "bn_regular_bg_items_bg.h"
 
 int main()
 {
-    // Initialization
-    // Make sure that all initialized Butano data types are placed
-    // AFTER this line is called.
-    bn::core::init();
+    bn::core::init();
 
-    // Plays the initial music on startup.
-    // ...
-    // Make sure that the 'music item' matches the name of an .xm or .mod file in /audio!
-    // The '1' represents your volume. Its data type is "bn::fixed".
-    // This means that you can put 'bn::fixed(0.5)' instead of '1'
-    // if you want it to play at half-volume.
-    bn::music_items::amayadori.play(1);
+    // Creates the background and sprites
+    bn::regular_bg_ptr bg = bn::regular_bg_items::bg.create_bg(0, 0);
+    bn::sprite_ptr paddle = bn::sprite_items::paddle.create_sprite(0, 60); // Moved paddle up slightly
+    paddle.set_rotation_angle(90);
 
-    // Create a background at position 0,0.
-    // We can move it around, but we don't want to for now.
-    // ...
-    // Notice that we're creating a "ptr" (pointer) object, which is
-    // populated with the output of an "item" object's create function.
-    // 'sprite_ptr' != 'sprite_item'
-    bn::regular_bg_ptr bg = bn::regular_bg_items::bg.create_bg(0, 0);
+    bn::sprite_ptr ball = bn::sprite_items::ball.create_sprite(0, 0);
 
-    // This creates the two paddles.
-    // The GBA resolution is 240 x 160, which means that, for a 64x64 paddle,
-    // You want their X values to be at about -140 and 140, respectively.
-    bn::sprite_ptr center_paddle = bn::sprite_items::paddle.create_sprite(0, 80);
+    // Initialize the game variables
+    int score = 0;
+    bn::fixed delta_x = 0;
+    bn::fixed delta_y = 0;
+    bool game_over = false;
+    bn::random random;
 
-    // We're flipping the right paddle, because the sprite is facing to the right.
-    // It's important to save on sprites anywhere we can:
-    // Don't forget: cartridges can only be around 16 MB in size!
-    center_paddle.set_rotation_angle(90);
+    // Constants
+    const int PADDLE_Y = 60;  // Fixed paddle Y position
+    const int PADDLE_WIDTH = 32;  // Half width of paddle
+    const int BALL_SIZE = 4;
+    const int SCREEN_TOP = -70;
+    const int SCREEN_LEFT = -120;
+    const int SCREEN_RIGHT = 120;
+    const int SCREEN_BOTTOM = 70;
 
-    // Let's put the ball in the center of the screen.
-    bn::sprite_ptr ball = bn::sprite_items::ball_new.create_sprite(0, 0);
+    // Setup the text display
+    bn::sprite_text_generator text_generator(common::variable_8x8_sprite_font);
+    bn::vector<bn::sprite_ptr, 16> text_sprites;
+    text_generator.generate(-6 * 16, -68, "(Press X to start)", text_sprites);
 
-    // Here are some variables to get us started.
-    int score = 0;               // We'll add a point when you score, and deduct a point when you fail.
-    bool enemy_going_up = false; // This will help us remember which way the enemy paddle should move.
+    // Vector in order to hold trace 
+    bn::vector<bn::sprite_ptr, 32> trace_sprites;
 
-    // 'Delta' means change.
-    // These two values represent, for each step of the gameloop,
-    // which direction the ball should go. Negative means towards top-left, and positive is bottom-right.
-    // For example,
-    // delta_x = -1, delta_y = 1, would mean that the ball is moving towards the bottom-left side of the screen.
-    // Increasing these numbers will make the ball move faster.
-    int delta_x = 0;
-    int delta_y = 0;
+    while(true)
+    {
+        // Paddle movement logic
+        if(bn::keypad::left_held() && paddle.x() > SCREEN_LEFT)
+        {
+            paddle.set_x(paddle.x() - 2);
+        }
+        else if(bn::keypad::right_held() && paddle.x() < SCREEN_RIGHT)
+        {
+            paddle.set_x(paddle.x() + 2);
+        }
 
-    // In Butano, as in many libraries, you need to initiatize an instance of a RANDOM object
-    // in order to get random numbers. This is what we're doing here.
-    bn::random random;
+        // Game over -> restart logic
+        if(game_over && bn::keypad::r_pressed())
+        {
+            ball.set_position(0, 0);
+            paddle.set_position(0, PADDLE_Y);
+            delta_x = 0;
+            delta_y = 0;
+            score = 0;
+            game_over = false;
 
-    // We'll want to set up a couple of Butano objects
-    // in order to put text on the screen.
-    bn::sprite_text_generator text_generator(common::variable_8x8_sprite_font);
+            text_sprites.clear();
+            text_generator.generate(-6 * 16, -68, "(Press X to start)", text_sprites);
+        }
 
-    // A 'vector' is a data type that allows us to add or take away elements on a stack,
-    // albeit a stack that can be accessed at any point. It's like an array, but it has
-    // a built-in "size" value so that we can see how many elements are in it.
-    // ..
-    // We need to set up a vector of sprite_ptr to represent individual letters.
-    // We can only have a max of 16 letters in this vector.
-    bn::vector<bn::sprite_ptr, 16> text_sprites;
+        // Start the game
+        if(bn::keypad::a_pressed() && delta_x == 0 && delta_y == 0 && !game_over)
+        {
+            text_sprites.clear();
+            bn::string<32> txt_score = "Score: " + bn::to_string<32>(score);
+            text_generator.generate(-6 * 16, -68, txt_score, text_sprites);
 
-    // Let's go ahead and set up our default text!
-    // The first two values represent X and Y.
-    // The third is your constant text.
-    // The fourth is your sprite_ptr vector.
-    text_generator.generate(-6 * 16, -68, "(Press A to start)", text_sprites);
+            // Initialize ball movement with random direction but always moving up initially
+            delta_x = (random.get_int() % 5) - 2;
+            delta_y = -2; // Start moving up
+            
+            bn::sound_items::pong.play();
+        }
 
-    // This is your main game loop.
-    // Remember, the GBA is ONLY running your game -
-    // You don't want it to be able to leave!
-    // That's why the loop is set to never end.
-    while (true)
-    {
+        // Ball's movement and collision logic
+        if(delta_x != 0 || delta_y != 0)
+        {
+            // Add a new dotted point to the trace at current ball position
+            if (trace_sprites.size() >= 32)  // Prevents overflow by limiting trace length
+            {
+                // Shifts elements to the front
+                for(int i = 1; i < trace_sprites.size(); ++i)
+                {
+                    trace_sprites[i - 1] = trace_sprites[i];
+                }
+                trace_sprites.pop_back();  
+            }
 
-        // If 'up' is being held, and we're not too far up,
-        // Take our Y position and set it relative to where we currently are,
-        // minus how far we want to move.
-        if (bn::keypad::left_held() && center_paddle.x() > -140)
-        {
-            center_paddle.set_x(center_paddle.x() - 1);
-        }
+            // Adds new trace point
+            trace_sprites.push_back(bn::sprite_items::ball.create_sprite(ball.x(), ball.y()));
 
-        // We use 'else' so that you can't hold both buttons down at
-        // the same time, hypothetically. This seems silly on real
-        // hardware, but you can do it in an emulator.
-        // ...
-        // Remember all potential use cases, to avoid bugs!
-        else if (bn::keypad::right_held() && center_paddle.x() < 140)
-        {
-            center_paddle.set_x(center_paddle.x() + 1);
-        }
+            // Updates the ball's position
+            ball.set_x(ball.x() + delta_x);
+            ball.set_y(ball.y() + delta_y);
 
-        // When the BALL IS STILL, and the A BUTTON IS PRESSED,
-        // We want to be able to set a new direction and speed for the ball.
-        // That's what this function is checking for.
-        if (bn::keypad::a_pressed() && delta_x == 0 && delta_y == 0)
-        {
+            // Top wall collision logic
+            if(ball.y() <= SCREEN_TOP)
+            {
+                ball.set_y(SCREEN_TOP);
+                delta_y = -delta_y;
+                bn::sound_items::pong.play();
+            }
 
-            // The 'generate' function fills up the selected vector,
-            // so make sure to clear whatever is in it!
-            text_sprites.clear();
+            // Side wall collisions logic
+            if(ball.x() <= SCREEN_LEFT)
+            {
+                ball.set_x(SCREEN_LEFT);
+                delta_x = -delta_x;
+                bn::sound_items::pong.play();
+            }
+            else if(ball.x() >= SCREEN_RIGHT)
+            {
+                ball.set_x(SCREEN_RIGHT);
+                delta_x = -delta_x;
+                bn::sound_items::pong.play();
+            }
 
-            // We're setting up a string to represent the new value.
-            bn::string<32> txt_score = "Score: " + bn::to_string<32>(score);
-            text_generator.generate(-6 * 16, -68, txt_score, text_sprites);
+            // Paddle collision logic
+            if(ball.y() + BALL_SIZE / 2 >= paddle.y() - PADDLE_WIDTH / 2 && ball.y() - BALL_SIZE / 2 <= paddle.y() + PADDLE_WIDTH  / 2) 
+            {
+                // Checks if ball is within paddle's width
+                if(ball.x() >= paddle.x() - PADDLE_WIDTH && ball.x() <= paddle.x() + PADDLE_WIDTH)
+                {
+                    // Bounces the ball off the top of the paddle
+                    ball.set_y(paddle.y() - (PADDLE_WIDTH / 2) - (BALL_SIZE / 2)); 
+                    delta_y = -delta_y;  
 
-            // In this function, I'm using a modulus.
-            // They're a little confusing, but I'll do my best to explain here.
-            // You can learn more about the modulus operator from here:
-            // https://www.mathsisfun.com/definitions/modulo-operation.html
-            // ...
-            // In short, the Modulus allows you to "wrap around" a number.
-            // Imagine that X represents a number counting up, and we were to
-            // operate under a modulus of 3.
-            // The output would look like this:
-            /*
+                    // Calculates where on the paddle the ball hit
+                    bn::fixed hit_position = (ball.x() - paddle.x()) / PADDLE_WIDTH;
 
-            x = 0       mod 3       output: 0
-            x = 1       mod 3       output: 1
-            x = 2       mod 3       output: 2
-            x = 3       mod 3       output: 0
-            x = 4       mod 3       output: 1
-            x = 5       mod 3       output: 2
+                    // Adjust the horizontal speed based on where the ball hits the paddle
+                    delta_x = hit_position * 3;  
 
-            */
-            // It's pretty easy to wrap your head around once you get the hang of it!
-            // Now... We're using it here, because the 'random.get_int()' function returns
-            // a random number that could be up to 4,294,967,295! However, if we modulus
-            // it to 4, then we will only get a random number between 0 and 3.
-            // ...
-            // I'm subtracting 2 because this means that, now, we have the potential to
-            // have the ball move in either direction.
-            // e.g.:
-            /*
+                    score++;
+                    text_sprites.clear();
+                    bn::string<32> txt_score = "Score: " + bn::to_string<32>(score);
+                    text_generator.generate(-6 * 16, -68, txt_score, text_sprites);
 
-            og number = 0       -2      = -2
-            og number = 1       -2      = -1
-            og number = 2       -2      = 0
-            og number = 4       -2      = 1
-            og number = 5       -2      = 2
+                    bn::sound_items::pong.play();
+                    
+                    // Resets the trace after ball hits the paddle
+                    trace_sprites.clear();
+                }
+            }
 
-            */
-            // Do you see how it allows us to have not only variable, speeds, but
-            // potential random movement in both directions?
-            // ...
-            // Now, I'm including a while loop around this logic, because
-            // there is always the potential chance that 'new_x' and 'new_y' will be 0,
-            // and we don't want that or the ball will stand still or move in a flat line!
+            // Ball goes past paddle -> game over
+            if(ball.y() >= SCREEN_BOTTOM && !game_over)
+            {
+                game_over = true;
+                ball.set_position(0, 0);
+                paddle.set_position(0, PADDLE_Y);
+                delta_x = 0;
+                delta_y = 0;
+                score = 0;
+                
+                text_sprites.clear();
+                text_generator.generate(-6 * 16, -68, "Game Over! Press S to restart", text_sprites);
 
-            while (delta_x == 0 || delta_y == 0)
-            {
-                delta_x = (random.get_int() % 5) - 2;
-                delta_y = (random.get_int() % 5) - 2;
-            }
+                // Clears trace on game over
+                trace_sprites.clear();
+            }
+        }
 
-            // Let's play a sound once it's done deciding which direction to go.
-            bn::sound_items::pong.play();
-        }
-
-        // Now, let's have the ball move according to our delta variables.
-        // ...
-        // You can also use '.set_position(x,y)' instead of setting x and y individually.
-        ball.set_x(ball.x() + delta_x);
-        ball.set_y(ball.y() + delta_y);
-
-        // I'm setting '-100' somewhat arbitrarily, after having
-        // played around with numbers to see what works.
-        // However, if the X value is less than -100 or more than 100,
-        // It's fair game for being well within the vacinity of a paddle.
-        if (ball.x() < -100)
-        {
-            delta_x = delta_x * -1;
-            bn::sound_items::pong.play();
-            // Remember - the 'point' at which a sprite is drawn is its center.
-            // We can find the distance between two sprites by calculating the average of their difference.
-            // ...
-            // We're using Y as the comparison between their Y position is what
-            // distinguishes between a 'pass' and a 'fail'.
-            // if (bn::abs(ball.y() - center_paddle.y()) < 32)
-            // {
-            //     // We made it!
-            //     // ...
-            //     // In math, multiplying by -1 reverses a number.
-            //     // So, this works to the effect of 'bouncing' the ball.
-            //     // Also, it plays a sound effect.
-            //     delta_x = delta_x * -1;
-            //     bn::sound_items::pong.play();
-            // }
-
-            // else
-            // {
-            //     // This executes if we didn't make it :(
-            //     //
-            //     // Let's lower the score by 1 and reset the ball.
-            //     score--;
-            //     ball.set_position(0, 0);
-            //     delta_x = 0;
-            //     delta_y = 0;
-
-            //     // I'm duplicating a lot of code here.
-            //     // Typically, I'd have pulled these sort of repeated blocks of code
-            //     // out into functions. However, I think it's important
-            //     // as a beginning to observer how everything works in-order.
-            //     text_sprites.clear();
-            //     bn::string<32> txt_score = "Score: " + bn::to_string<32>(score) + " (Press A)";
-            //     text_generator.generate(-6 * 16, -68, txt_score, text_sprites);
-            // }
-        }
-
-        // This is the same as the above, but for when the ball goes too far
-        // to the right. This is what happens when it goes up against
-        // the CPU player.
-        else if (ball.x() > 100)
-        {
-            delta_x = delta_x * -1;
-            bn::sound_items::pong.play();
-            // score++;
-            // ball.set_position(0, 0);
-            // delta_x = 0;
-            // delta_y = 0;
-            // text_sprites.clear();
-            // bn::string<32> txt_score = "Score: " + bn::to_string<32>(score) + " (Press A)";
-            // text_generator.generate(-6 * 16, -68, txt_score, text_sprites);
-        }
-
-        // This is for when the ball hits the top of the screen.
-        if (ball.y() < -70)
-        {
-            delta_y = delta_y * -1;
-            bn::sound_items::ping.play();
-        }
-
-        if (ball.x() > (center_paddle.x() - 30) && ball.x() < (center_paddle.x() + 30))  // Horizontal collision zone
-        {
-            // Check if ball is at the paddle's vertical position
-            if (bn::abs(ball.y() == 50))  // Vertical collision zone
-            {   
-                BN_LOG(ball.y());
-                BN_LOG(center_paddle.y());
-                // Increase score
-                score++;
-                text_sprites.clear();
-                bn::string<32> txt_score = "Score: " + bn::to_string<32>(score);
-                text_generator.generate(-6 * 16, -68, txt_score, text_sprites);
-
-                // Simple bounce - invert the vertical direction
-                delta_y = -delta_y;
-                
-                
-                // Play sound effect
-                bn::sound_items::pong.play();
-            }
-        }
-
-        // ...and for when the ball hits the bottom of the screen.
-        else if (ball.y() > 70)
-        {
-            score = 0;
-            ball.set_position(0, 0);
-            delta_x = 0;
-            delta_y = 0;
-            text_sprites.clear();
-            bn::string<32> txt_score = "Score: " + bn::to_string<32>(score) + " (Press A)";
-            text_generator.generate(-6 * 16, -68, txt_score, text_sprites);
-        }
-
-        // One last thing!
-        // Let's talk about LOGGING.
-        // We'll have the Butano logs add your score to the log when you click 'B'.
-        // You can view these from within mGBA by selecting:
-        /*
-
-        1. Tools
-        2. View logs
-
-        */
-        // This is ABSOLUTELY ESSENTIAL for debugging.
-
-        if (bn::keypad::b_pressed())
-        {
-            BN_LOG(score);
-        }
-
-        // Do all the Butano things that we need to have done in the background.
-        // If you don't call this, nothing will happen on the screen or through the speakers.
-        bn::core::update();
-    }
+        bn::core::update();
+    }
 }
-
-// And now we're done :)
-// Good job!
-
-// and (bn::abs(ball.x() - center_paddle.x()) < 2)
